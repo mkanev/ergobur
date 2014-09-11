18c431cf852a566bbd65bce71ab8b884c3becbe8
Maksim Kanev <maksim.kanev@gmail.com>
2014-09-12 00:43:35 +0400

    Init with Makers

---
+++ src/main/webapp/js/custom.js
@@ -0,0 +1,58 @@
+/* ==============================================
+			Sticky Header
+ =============================================== */
+
+
+    	jQuery(".navbar").sticky({
+    		topSpacing: 0
+    	});
+
+
+/* ==============================================
+			Smooth scroll navigation
+ =============================================== */
+ 
+	
+     $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+	
+	$('.navbar-collapse ul li a').click(function(){ 
+  $('.navbar-toggle:visible').click();
+});	
+
+
+
+/* ==============================================
+			Page loader
+ =============================================== */
+ 
+ 
+ $(window).load(function() {
+	$(".loader-item").delay(500).fadeOut();
+	$("#pageloader").delay(1000).fadeOut("slow");
+	});
+
+
+
+/* ==============================================
+			Scroll to top
+ =============================================== */
+	 
+	$(window).scroll(function(){
+                    if ($(this).scrollTop() > 100) {
+                        $('.scrollup').fadeIn();
+                    } else {
+                        $('.scrollup').fadeOut();
+                    }
+                }); 
+         
+                $('.scrollup').click(function(){
+                    $("html, body").animate({ scrollTop: 0 }, 2000);
+                    return false;
+                });
+   
