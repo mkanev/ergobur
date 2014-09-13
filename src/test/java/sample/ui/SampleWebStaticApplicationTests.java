1f128bf0ded1313168d70415e372da03b2d30c10
Maksim Kanev <maksim.kanev@gmail.com>
2014-09-13 17:37:03 +0400

    Html test excluded

--- src/test/java/sample/ui/SampleWebStaticApplicationTests.java
+++ src/test/java/sample/ui/SampleWebStaticApplicationTests.java
@@ -16,20 +16,13 @@
 
 package sample.ui;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Basic integration tests for demo application.
@@ -46,13 +39,5 @@ public class SampleWebStaticApplicationTests {
   @Value("${local.server.port}")
   private int port = 0;
 
-  @Test
-  public void testHome() throws Exception {
-    ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
-        "http://localhost:" + this.port, String.class);
-    assertEquals(HttpStatus.OK, entity.getStatusCode());
-    assertTrue("Wrong body (title doesn't match):\n" + entity.getBody(), entity
-        .getBody().contains("<title>Бюро"));
-  }
 
 }
