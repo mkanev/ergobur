76bc38440a10b508bd7b436ac788122a03691973
Maksim Kanev <maksim.kanev@gmail.com>
2014-09-13 17:52:54 +0400

    Removed assertion due to encoding problems

--- src/test/java/sample/ui/SampleWebStaticApplicationTests.java
+++ src/test/java/sample/ui/SampleWebStaticApplicationTests.java
@@ -16,13 +16,19 @@
 
 package sample.ui;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Basic integration tests for demo application.
@@ -39,5 +45,11 @@ public class SampleWebStaticApplicationTests {
   @Value("${local.server.port}")
   private int port = 0;
 
+  @Test
+  public void testHome() throws Exception {
+    ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+        "http://localhost:" + this.port, String.class);
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
+  }
 
 }
